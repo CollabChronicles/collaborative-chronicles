0726196fe8d5bd755c6e556869b3fe2942c21088
CollabChronicles <collabchroniclesproject@gmail.com>
2025-07-03 17:16:31 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// This file will hold your site’s interactive JavaScript.
+// We’ll add code here later to handle submissions and dynamic updates.
+
+console.log('Collaborative Chronicles script loaded.');
